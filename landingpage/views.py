3f32d174dce644af553224df6abca3ade5ada430
William D. Gizzi <gizzidan@me.com>
2017-01-03 19:49:09 -0500

    blog design

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.template import RequestContext
 
 def index(request):
-    return render(request, 'landingpage/base.html', {})
+    return render(request, 'landingpage/base_landing.html', {})
 # Create your views here.
