10a9bd13041b703f8da1adb686032e163b4cd996
William D. Gizzi <gizzidan@me.com>
2017-01-06 11:40:56 -0500

    adding slug pre migration

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,40 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    FITNESS = 'F'
+    NUTRITION = 'N'
+    LIFESTYLE = 'L'
+    PRODUCTIVITY = 'P'
+    ENTREPRENEURSHIP = 'E'
+    CATEGORY_CHOICES = (
+        (FITNESS, 'Fitness'),
+        (NUTRITION, 'Nutrition'),
+        (LIFESTYLE, 'Lifestyle'),
+        (PRODUCTIVITY, 'Productivity'),
+        (ENTREPRENEURSHIP, 'Entrepreneurship'),
+    )
+
+    name = models.CharField(choices=CATEGORY_CHOICES, blank=False, max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
+    slug = models.SlugField(editable=False, unique=True)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('article', (), {
+            'slug': self.slug,
+            'id': self.id,
+        })
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,5 +42,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_detail(request, slug, id):
+    post = get_object_or_404(Post, pk=id)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
