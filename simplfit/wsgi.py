ed5c3a09527c8147587b52a7ca1e094d1deb4c8b
William D. Gizzi <gizzidan@me.com>
2017-01-23 15:29:11 -0500

    ok

--- simplfit/wsgi.py
+++ simplfit/wsgi.py
@@ -11,7 +11,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplfit.settings")
-
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
