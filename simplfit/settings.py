621fd46fc2d778a5759a566866a2770b0f6db71e
William D. Gizzi <gizzidan@me.com>
2017-02-18 17:38:15 -0500

    ok

--- simplfit/settings.py
+++ simplfit/settings.py
@@ -176,3 +176,13 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+if not DEBUG:
+    AWS_QUERYSTRING_AUTH = False
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_HOST = "s3-us-east-2.amazonaws.com"
+    AWS_S3_SECURE_URLS = False       # use http instead of https
+    MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
