8ad2a375d9b234242ff49fc5bce40856a98f8b18
William D. Gizzi <gizzidan@me.com>
2017-02-22 12:11:40 -0500

    ok

--- simplfit/settings.py
+++ simplfit/settings.py
@@ -176,16 +176,13 @@ try:
 except ImportError:
     pass
 
-if DEBUG:
-    MEDIA_URL = '/media/'
-    
-else:
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_SECURE_URLS = False       # use http instead of https
-    AWS_DEFAULT_REGION = 'us-east-2'
-    AWS_S3_HOST = "s3.us-east-2.amazonaws.com"  # Change to the media center you chose when creating the bucket
-    MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-    DEFAULT_FILE_STORAGE = 'simplfit.s3utils.MediaS3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_SECURE_URLS = False       # use http instead of https
+AWS_DEFAULT_REGION = 'us-east-2'
+AWS_S3_HOST = "s3.us-east-2.amazonaws.com"  # Change to the media center you chose when creating the bucket
+MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = 'simplfit.s3utils.MediaS3BotoStorage'
